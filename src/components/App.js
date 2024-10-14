782c0b8e8fa3e8edb4aa95bf6e103f6cfdc0b077
Qajay0832 <qajay0832@gmail.com>
2024-10-14 18:14:23 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
 
-import React from "react";
+import React ,{useState,useEffect,useMemo} from "react";
 import './../styles/App.css';
+import axios from 'axios';
 
 const App = () => {
+  const [data,setData]=useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const fetchApi = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        setData(response.data)
+      
+    } catch (err) {
+      setError('Error fetching data');
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchApi();
+  }, []);
+  
+  const memoizedData = useMemo(() => data, [data]);
   return (
     <div>
         {/* Do not remove the main div */}
+        {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {!loading && !error && (
+        <ul>
+          {memoizedData.map(post => (
+            <li key={post.id}>
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
